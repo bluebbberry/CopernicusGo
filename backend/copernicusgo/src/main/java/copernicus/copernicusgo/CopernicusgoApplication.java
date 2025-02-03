7093f1e0336e831db460b1c50c67e8340563cf79
blueberry <jnb@campus.uni-paderborn.de>
2025-02-03 20:49:21 +0100

    add backend code

---
+++ backend/copernicusgo/src/main/java/copernicus/copernicusgo/CopernicusgoApplication.java
@@ -0,0 +1,13 @@
+package copernicus.copernicusgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopernicusgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopernicusgoApplication.class, args);
+	}
+
+}
---
+++ backend/copernicusgo/src/test/java/copernicus/copernicusgo/CopernicusgoApplicationTests.java
@@ -0,0 +1,13 @@
+package copernicus.copernicusgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopernicusgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
